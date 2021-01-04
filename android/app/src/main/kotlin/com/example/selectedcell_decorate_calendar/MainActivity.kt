6f78a7c5d69409d9cd3ef14d33fd67aa8bf76a6e
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2021-01-04 17:24:56 +0530

    FLUT-3345 - Sample and screenshot added.

---
+++ android/app/src/main/kotlin/com/example/selectedcell_decorate_calendar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.selectedcell_decorate_calendar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+void main() => runApp(SelectionDecoration());
+
+class SelectionDecoration extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            body: SafeArea(
+                  child: SfCalendar(
+                    view: CalendarView.day,
+                    allowedViews: <CalendarView>[
+                      CalendarView.day,
+                      CalendarView.week,
+                      CalendarView.workWeek,
+                      CalendarView.month,
+                      CalendarView.timelineDay,
+                      CalendarView.timelineWeek,
+                      CalendarView.timelineWorkWeek,
+                      CalendarView.timelineMonth,
+                    ],
+                    selectionDecoration: BoxDecoration(
+                      color: Colors.purpleAccent,
+                      border: Border.all(color: Colors.deepPurple, width: 2),
+                      borderRadius: const BorderRadius.all(Radius.circular(4)),
+                      shape: BoxShape.rectangle,
+
+                    ),
+                  )),
+            ));
+  }
+}
