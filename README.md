d7d539e0161fb38f95023515ffbaf0cdace2d210
Muniappan <muniappan.subramanian@syncfusiopn.com>
2022-11-22 17:37:42 +0530

    sample committed

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-**[View document in our Syncfusion Flutter knowledge base](https://www.syncfusion.com/kb/12245/how-to-customize-the-selection-using-decoration-in-the-flutter-event-calendar-sfcalendar)**
+# selectedcell_decorate_calendar
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 
-void main() => runApp(SelectionDecoration());
+void main() => runApp(const SelectionDecoration());
 
 class SelectionDecoration extends StatelessWidget {
+  const SelectionDecoration({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-            body: SafeArea(
-                  child: SfCalendar(
-                    view: CalendarView.day,
-                    allowedViews: <CalendarView>[
-                      CalendarView.day,
-                      CalendarView.week,
-                      CalendarView.workWeek,
-                      CalendarView.month,
-                      CalendarView.timelineDay,
-                      CalendarView.timelineWeek,
-                      CalendarView.timelineWorkWeek,
-                      CalendarView.timelineMonth,
-                    ],
-                    selectionDecoration: BoxDecoration(
-                      color: Colors.purpleAccent,
-                      border: Border.all(color: Colors.deepPurple, width: 2),
-                      borderRadius: const BorderRadius.all(Radius.circular(4)),
-                      shape: BoxShape.rectangle,
+          body: SafeArea(
+              child: SfCalendar(
+                view: CalendarView.day,
+                allowedViews: const <CalendarView>[
+                  CalendarView.day,
+                  CalendarView.week,
+                  CalendarView.workWeek,
+                  CalendarView.month,
+                  CalendarView.timelineDay,
+                  CalendarView.timelineWeek,
+                  CalendarView.timelineWorkWeek,
+                  CalendarView.timelineMonth,
+                ],
+                selectionDecoration: BoxDecoration(
+                  color: Colors.purpleAccent,
+                  border: Border.all(color: Colors.deepPurple, width: 2),
+                  borderRadius: const BorderRadius.all(Radius.circular(4)),
+                  shape: BoxShape.rectangle,
 
-                    ),
-                  )),
-            ));
+                ),
+              )),
+        ));
   }
 }
